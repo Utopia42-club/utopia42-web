f0a86bc7fbf38db59c2bdc2f1d943e494b9e9abd
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-04 20:41:16 +0330

    Fix minor problems with plugin inputs form

--- src/app/utopia-game/plugin-dialog/filter-input-options.pipe.ts
+++ src/app/utopia-game/plugin-dialog/filter-input-options.pipe.ts
@@ -6,7 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterInputOptionsPipe implements PipeTransform {
 
     transform(options: { key: string, value: string }[], filter: string): unknown {
-        return options.filter(option => option.value.toLowerCase().includes(filter.toLowerCase()));
+        if (filter == null) {
+            return options;
+        }
+        return options.filter(option => option.key.toLowerCase().includes((filter + "").toLowerCase()));
     }
 
 }
