75a3b8224721e205f03fd1f1c251cb4a2fde8af2
Navid Farokhi <mojtaba.farokhi99@gmail.com>
2022-02-22 11:51:41 +0330

    Enhance support for reviewing plugin source code, before execution (#7)

--- src/app/utopia-game/plugin/plugin-confirmation-dialog/plugin-confirmation-dialog.component.ts
+++ src/app/utopia-game/plugin/plugin-confirmation-dialog/plugin-confirmation-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Plugin } from '../Plugin';
+import { PluginExecutionService } from '../plugin-execution.service';
+import * as Prism from 'prismjs';
 
 @Component({
     selector: 'app-plugin-confirmation-dialog',
@@ -11,9 +13,15 @@ import { Plugin } from '../Plugin';
 export class PluginConfirmationDialog implements OnInit {
     plugin: Plugin;
     acceptedTerms = false;
+    code: any;
 
-    constructor(readonly dialogRef: MatDialogRef<PluginConfirmationDialog>, @Inject(MAT_DIALOG_DATA) readonly data: any,) {
+    constructor(readonly dialogRef: MatDialogRef<PluginConfirmationDialog>, @Inject(MAT_DIALOG_DATA) readonly data: any,
+                readonly pluginExecutionService: PluginExecutionService) {
         this.plugin = data.plugin;
+        pluginExecutionService.getFile(this.plugin.scriptUrl)
+            .subscribe(code => {
+                this.code = Prism.highlight(code, Prism.languages.js, 'js');
+            });
     }
 
 
