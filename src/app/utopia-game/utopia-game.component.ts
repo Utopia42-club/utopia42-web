ace01dade27142cfd1dc3cca07540e87f925dc93
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-04 18:26:16 +0330

    Update versions and url for main server deployment

--- src/app/utopia-game/utopia-game.component.ts
+++ src/app/utopia-game/utopia-game.component.ts
@@ -127,7 +127,7 @@ export class UtopiaGameComponent implements OnInit, OnDestroy {
 
 
     private startGame() {
-        let buildUrl = '/assets/game/v0.12-rc5/Build';
+        let buildUrl = '/assets/game/v0.12/Build';
         let config = {
             dataUrl: buildUrl + '/web.data',
             frameworkUrl: buildUrl + '/web.framework.js',
@@ -135,7 +135,7 @@ export class UtopiaGameComponent implements OnInit, OnDestroy {
             streamingAssetsUrl: 'StreamingAssets',
             companyName: 'Utopia 42',
             productName: 'Utopia 42',
-            productVersion: '0.12-rc5',
+            productVersion: '0.12',
             showBanner: (m, t) => this.showBanner(m, t),
         };
 
