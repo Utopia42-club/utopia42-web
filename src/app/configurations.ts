8aab1c54d4a0da3d626c39d204b17ecc4acc2dd6
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-12 22:11:21 +0330

    Update versions and url

--- src/app/configurations.ts
+++ src/app/configurations.ts
@@ -1,6 +1,6 @@
 export class Configurations {
-    public static readonly SERVER_URL = 'https://utopiapi.vitaminhq.ir';
-    // public static readonly SERVER_URL = "https://api.utopia42.club";
+    // public static readonly SERVER_URL = 'https://utopiapi.vitaminhq.ir';
+    public static readonly SERVER_URL = "https://api.utopia42.club";
     public static readonly NETS_URL = Configurations.SERVER_URL + '/static/networks.json';
     public static readonly WINDOWS_URL = Configurations.SERVER_URL + '/static/windows.zip';
 
