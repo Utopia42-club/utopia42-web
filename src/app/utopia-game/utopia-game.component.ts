ddb357b0cb8c2df3fa8f8fad010d5068a82b616f
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-06-20 10:14:14 +0430

    Update versions

--- src/app/utopia-game/utopia-game.component.ts
+++ src/app/utopia-game/utopia-game.component.ts
@@ -138,7 +138,7 @@ export class UtopiaGameComponent implements OnInit, OnDestroy {
     }
 
     private startGame() {
-        let buildUrl = '/assets/game/0.20-rc12/Build';
+        let buildUrl = '/assets/game/0.20-rc13/Build';
         let loaderUrl = buildUrl + '/web.loader.js';
         let config = {
             dataUrl: buildUrl + '/web.data',
@@ -147,7 +147,7 @@ export class UtopiaGameComponent implements OnInit, OnDestroy {
             streamingAssetsUrl: 'StreamingAssets',
             companyName: 'Utopia 42',
             productName: 'Utopia 42',
-            productVersion: '0.20-rc12',
+            productVersion: '0.20-rc13',
             showBanner: (m, t) => this.showBanner(m, t),
         };
 
