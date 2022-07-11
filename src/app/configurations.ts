65005156d06b61911966418c981270e8d08d795b
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-07-11 12:06:39 +0430

    Update urls and versions for main server deploy

--- src/app/configurations.ts
+++ src/app/configurations.ts
@@ -1,10 +1,10 @@
 export class Configurations {
-    public static readonly SERVER_URL = 'https://demoapi.utopia42.club';
-    public static readonly WS_SERVER_URL = 'wss://demoapi.utopia42.club';
+    // public static readonly SERVER_URL = 'https://demoapi.utopia42.club';
+    // public static readonly WS_SERVER_URL = 'wss://demoapi.utopia42.club';
     // public static readonly SERVER_URL = 'https://utopiapi.vitaminhq.ir';
     // public static readonly WS_SERVER_URL = 'wss://utopiapi.vitaminhq.ir';
-    // public static readonly SERVER_URL = "https://api.utopia42.club";
-    // public static readonly WS_SERVER_URL = "wss://api.utopia42.club";
+    public static readonly SERVER_URL = "https://api.utopia42.club";
+    public static readonly WS_SERVER_URL = "wss://api.utopia42.club";
     // public static readonly SERVER_URL = "http://127.0.0.1:8080";
     public static readonly NETS_URL = Configurations.SERVER_URL + '/static/networks.json';
     public static readonly WINDOWS_URL = Configurations.SERVER_URL + '/static/windows.zip';
